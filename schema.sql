6cea8a7228e7bd96e102b421b098df7d7c8b6e4d
Valentina-Zhmachenko <72689210+Valentina-Zhmachenko@users.noreply.github.com>
2020-10-12 18:47:23 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,48 +1,48 @@
 create schema clinic;
 
 create table clinic.patient(
-    id bigserial primary key,
-    full_name text,
-    birthdate date,
-    phone_number int
+    id 			bigserial primary key,
+    full_name 		text not null,
+    birthdate 		date,
+    phone_number 	character varying(11) unique 
 );
 
-create table clinic.doctor_visit(
-	id bigserial primary key,
-	doctor_id bigserial,
-	patient_id bigserial,
-	service_id bigserial,
-	visit_date date,
-	price money,
-	constraint fk_doctor foreign key(doctor_id) references doctor(id),
-	constraint fk_patient foreign key(patient_id) references patient(id),
-	constraint fk_service foreign key(service_id) references service(id)
-);
-
-create table clinic.service(
-	id bigserial primary key,
-	service text,
-	price money
-);
-
-create table clinic.doctor(
-	id bigserial primary key,
-	position_id bigserial,
-	department_id bigserial,	
-	full_name text,
-	phone_number int,
-	constraint fk_position foreign key(position_id) references position(id),
-	constraint fk_department foreign key(department_id) references department(id),
-);
-
-create table clinic.position(
-	id bigserial primary key,
-	position_name text,
-	description text
+create table clinic.position_doctor(
+	id 		bigserial primary key,
+	position_name 	text not null,
+	description 	text not null
 );
 
 create table clinic.department(
-	id bigserial primary key,
-	department_name text,
-	floor_number int
+	id 		bigserial primary key,
+	department_name text not null,
+	floor_number 	int not null default 1 check(floor_number > 0 and floor_number < 16)
+);
+
+create table clinic.service(
+	id 		bigserial primary key,
+	service 	text not null,
+	price 		money not null
+);
+
+create table clinic.doctor(
+	id 		bigserial primary key,
+	position_id 	int,
+	department_id 	int,	
+	full_name 	text not null,
+	phone_number 	int unique not null,
+	constraint fk_position foreign key(position_id) references clinic.position_doctor(id),
+    constraint fk_department foreign key(department_id) references clinic.department(id)
+);
+
+create table clinic.doctor_visit(
+	id              bigserial primary key,
+	doctor_id 	int,
+	patient_id 	int,
+	service_id 	int,
+	visit_date 	date not null,
+	price 		money not null,
+	constraint fk_doctor foreign key(doctor_id) references clinic.doctor(id),
+	constraint fk_patient foreign key(patient_id) references clinic.patient(id),
+    constraint fk_service foreign key(service_id) references clinic.service(id)
 );
