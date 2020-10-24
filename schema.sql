2ee871b8adcc9f5def414ba8facc7a4bf9012110
Valentina-Zhmachenko <72689210+Valentina-Zhmachenko@users.noreply.github.com>
2020-10-24 13:15:19 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,49 +1,49 @@
 create schema clinic;
 
 create table clinic.patient(
-    id 				bigserial primary key,
+    id 			bigserial primary key,
     full_name 		text not null,
     birthday 		date,
     phone_number 	character varying(11) unique 
 );
 
 create table clinic.position_doctor(
-	id 				bigserial primary key,
-	position_name 	text not null,
-	description 	text not null
+    id 		        bigserial primary key,
+    position_name 	text not null,
+    description 	text not null
 );
 
 create table clinic.department(
-	id 				bigserial primary key,
-	department_name text not null,
-	floor_number 	int not null default 1 check(floor_number > 0 and floor_number < 16)
+    id 			bigserial primary key,
+    department_name     text not null,
+    floor_number 	int not null default 1 check(floor_number > 0 and floor_number < 16)
 );
 
 create table clinic.service(
-	id 				bigserial primary key,
-	service 		text not null,
-	price 			money not null
+    id 			bigserial primary key,
+    service 		text not null,
+    price 		money not null
 );
 
 create table clinic.doctor(
-	id 				bigserial primary key,
-	position_id 	bigserial,
-	department_id 	bigserial,	
-	full_name 		text not null,
-	phone_number 	character varying(11) unique not null,
-	constraint fk_position foreign key(position_id) references clinic.position_doctor(id),
+    id 			bigserial primary key,
+    position_id 	bigserial,
+    department_id 	bigserial,	
+    full_name 		text not null,
+    phone_number 	character varying(11) unique not null,
+    constraint fk_position foreign key(position_id) references clinic.position_doctor(id),
     constraint fk_department foreign key(department_id) references clinic.department(id)
 );
 
 create table clinic.doctor_visit(
-	id              bigserial primary key,
-	doctor_id 		bigserial,
-	patient_id 		bigserial,
-	service_id 		bigserial,
-	visit_date 		date not null,
-	price 			money not null,
-	constraint fk_doctor foreign key(doctor_id) references clinic.doctor(id),
-	constraint fk_patient foreign key(patient_id) references clinic.patient(id),
+    id                  bigserial primary key,
+    doctor_id 		bigserial,
+    patient_id 		bigserial,
+    service_id 		bigserial,
+    visit_date 		date not null,
+    price 		money not null,
+    constraint fk_doctor foreign key(doctor_id) references clinic.doctor(id),
+    constraint fk_patient foreign key(patient_id) references clinic.patient(id),
     constraint fk_service foreign key(service_id) references clinic.service(id)
 );
 
